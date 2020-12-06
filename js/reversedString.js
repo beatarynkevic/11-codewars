4e5d0afd43a963fde92ebfdb390ec1130df767ff
beatarynkevic <beatainn@gmail.com>
2020-12-06 21:02:49 +0200

    codewars

---
+++ js/reversedString.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+function solution(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(solution('world'));
+
+//   https://dev.to/valec/4-different-ways-to-reverse-a-string-with-javascript-34cl
